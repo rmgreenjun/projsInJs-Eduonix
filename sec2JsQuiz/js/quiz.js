0d0ce4b1718e8cea00df7683ef0dbed6e1070b40
Rich Green <rmgreenjun@gmail.com>
2017-04-16 13:56:06 -0400

    update quiz files

---
+++ sec2JsQuiz/js/quiz.js
@@ -0,0 +1,14 @@
+
+function submitAnswers() {
+	var total = 5;
+	var score = 0;
+	
+	//get user input
+	var q1 = document.forms["quizForm"]["q1"].value;
+	var q2 = document.forms["quizForm"]["q2"].value;
+	var q3 = document.forms["quizForm"]["q3"].value;
+	var q4 = document.forms["quizForm"]["q4"].value;
+	var q5 = document.forms["quizForm"]["q5"].value;
+	
+	alert(q1);
+}
